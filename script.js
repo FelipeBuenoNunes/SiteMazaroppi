24430ddd291109b2d69ce887ad891e7baa6fdb36
ReySJS <78664426+ReySJS@users.noreply.github.com>
2021-11-17 17:12:55 -0300

    Feedback
    Excelente Trabalho pessoal!

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-function iniciaModal(modalID) {
-    const modal = document.getElementById(modalID);
-    if (modal) {
-        modal.classList.add('mostrar');
-        modal.addEventListener('click', (e) => {
-            if (e.target.id == modalID || e.target.classList == 'button') {
-                modal.classList.remove('mostrar');
-            }
-        });
-    }
+// function iniciaModal(modalID) {
+//     const modal = document.getElementById(modalID);
+//     if (modal) {
+//         modal.classList.add('mostrar');
+//         modal.addEventListener('click', (e) => {
+//             if (e.target.id == modalID || e.target.classList == 'button') {
+//                 modal.classList.remove('mostrar');
+//             }
+//         });
+//     }
+// };
+
+// const logo = document.querySelector('.filter');
+// logo.addEventListener('click', () => iniciaModal('modal-promocao'));
+
+
+
+//=> Uma forma de abrir e fechar o modal
+document.querySelector(".filter").addEventListener("click", toggleModal);
+
+function toggleModal() {
+  const modal = document.getElementById("modal-promocao");
+  modal.classList.toggle("mostrar");
 }
-
-const logo = document.querySelector('.filter');
-logo.addEventListener('click', () => iniciaModal('modal-promocao'));
-
